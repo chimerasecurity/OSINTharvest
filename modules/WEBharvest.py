99c797f44bf7d8c59c8f5ee0b94ae4ea1eeffbd6
chimerasecurity <Josh@MBP.local>
2013-11-10 22:09:55 +0000

    WEBharvest

--- modules/WEBharvest.py
+++ modules/WEBharvest.py
@@ -12,8 +12,14 @@
 import re           # required for regex
 import urllib2      # required for http
 
+# Determine regex for IP / HNAMEs
+IP = r"\b(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\b"
+#HNAME = 
+
+# src defines the source web page, possibly change for user input?
 src = urllib2.urlopen("http://blog.threatexpert.com/2010/01/trojanhydraq-part-ii.html")
 data = src.read()
 
-result = re.compile(r"\b(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\b")
+# search and print matching strings
+result = re.compile(IP)
 print result.findall(data)
