1c28b23de8fa3472ed621a3a0edf2b041bd849a4
chimerasecurity <Josh@MBP.local>
2013-11-10 22:00:37 +0000

    WEBharvest initial

--- modules/WEBharvest.py
+++ modules/WEBharvest.py
@@ -7,3 +7,13 @@
 # Module: WEBharvest
 # Version: 0.1 (20/09/13)
 # Readme: ../OSINTharvest/Readme.txt
+
+# Import required libs
+import re           # required for regex
+import urllib2      # required for http
+
+src = urllib2.urlopen("http://blog.threatexpert.com/2010/01/trojanhydraq-part-ii.html")
+data = src.read()
+
+result = re.compile(r"\b(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\b")
+print result.findall(data)
