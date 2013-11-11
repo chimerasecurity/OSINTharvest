85513f78cadcf6f22afd41aa856233aa670f5eaa
chimerasecurity <Josh@MBP.local>
2013-11-11 22:35:48 +0000

    Update

--- modules/WEBharvest.py
+++ modules/WEBharvest.py
@@ -8,18 +8,56 @@
 # Version: 0.1 (20/09/13)
 # Readme: ../OSINTharvest/Readme.txt
 
-# Import required libs
+# Import required li
 import re           # required for regex
 import urllib2      # required for http
+import datetime     # required for timestmps
+import os
+import csv
+
+OUTPUT_FILE = '../output/WEBharvest/results.csv'   # Output directory
+SOURCELIST = '../sources/WEBharvest_sources.txt'   # Sourcelist
+LOGFILE = '../logs/OSINTharvest.log'               # logs
+now = datetime.datetime.today()
 
 # Determine regex for IP / HNAMEs
 IP = r"\b(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\b"
-#HNAME = 
 
-# src defines the source web page, possibly change for user input?
-src = urllib2.urlopen("http://blog.threatexpert.com/2010/01/trojanhydraq-part-ii.html")
-data = src.read()
+# Open up the log file, write the header
+f = open(LOGFILE, 'w')
 
-# search and print matching strings
-result = re.compile(IP)
-print result.findall(data)
+
+# write the module launch to log w/timestamp
+f.write( now.strftime("%Y-%m-%d %H:%M:%S") + " - WEBharvest" + " - Module launched")
+
+# print launch message
+print("Launching WEBharvest. Please wait..")
+
+# For every line in the source, do the following
+for source in open(SOURCELIST):
+
+    # Define <now>
+    now = datetime.datetime.today()
+    
+    # Grab the filename from the source list
+    name = source.rsplit(',', 1)[-1]
+    
+    # Define the url
+    url = source.rsplit(',', 1)[-0]
+    
+    # Scrape
+    src = urllib2.urlopen(url)
+    data = src.read()
+    
+    # search and print matching strings
+    result = re.compile(IP)
+    h = result.findall(data)
+    
+    csv.writer(open(OUTPUT_FILE, 'wb')).writerows(h)
+    
+    # Show outcome of download
+    f.write( now.strftime("%Y-%m-%d %H:%M:%S") + " - WEBharvest" + " - Scrape Successful: " + name)
+
+
+
+
